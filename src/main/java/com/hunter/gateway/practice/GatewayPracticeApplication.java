076f3a39d15deeebe19614242444a7d836fb9dd5
高伟 <hunter_gw@163.com>
2021-11-22 11:27:40 +0800

    练习内容提交

---
+++ src/main/java/com/hunter/gateway/practice/GatewayPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.hunter.gateway.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPracticeApplication.class,args);
+    }
+}
